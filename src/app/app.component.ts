6545fa07ffe578d5fc236e87a45e18d8963cb330
AvidDollars <dima.kol@seznam.cz>
2025-06-03 11:44:41 +0200

    add basic breakpoints for sm/md/lg screens + init header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  host: {
+    class: `
+      m-auto mt-8 rounded-lg border-2 h-[90vh]
+      max-w-[1200px]
+      sm:w-[100vw]
+      md:w-[80vw]
+      lg:w-[70vw]
+    `
+  }
 })
 export class AppComponent {
   title = 'form_demo';
