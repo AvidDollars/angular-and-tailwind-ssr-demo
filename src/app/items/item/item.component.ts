c982bde10573365ea84185f6f3d95eabf0d45911
AvidDollars <dima.kol@seznam.cz>
2025-06-03 15:40:52 +0200

    create basic layout for card items and its container

---
+++ src/app/items/item/item.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  imports: [],
+  templateUrl: './item.component.html',
+  styleUrl: './item.component.css',
+  host: {
+    class: `
+      h-[300px]
+      bg-green-800
+      p-2
+    `
+  }
+})
+export class ItemComponent {
+
+}
