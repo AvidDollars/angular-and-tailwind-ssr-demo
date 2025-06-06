823ffbe694eaa98d9c03fee62d8ca9e4f88a6b51
AvidDollars <dima.kol@seznam.cz>
2025-06-06 10:15:29 +0200

    create routes for shopping and payment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { ItemContainerComponent } from './items/item-container/item-container.component';
 import { Routes } from '@angular/router';
+import { PaymentComponent } from './payment/payment.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: ItemContainerComponent,
+  },
+  {
+    path: "payment",
+    component: PaymentComponent,
+  }
+];
